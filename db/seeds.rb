dfbd1c7e0d35b3d44af21563ed56122896ba430e
alexzhou415 <zhoualex10@yahoo.com>
2021-04-08 09:12:19 -0700

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,11 @@
 User.delete_all
 
 u1 = User.create!(username: 'DemoUser', email: 'demo@email.com', password:'12345678', location: 'Richmond, CA')
+u1 = User.create!(username: 'JRich', email: 'jjsmith@email.com', password:'hotdog64', location: 'San Diego, CA')
+u1 = User.create!(username: 'StevenAdams', email: 'newzealander@email.com', password:'matemate', location: 'New York, NY')
+u1 = User.create!(username: 'ChefCurry', email: '40percent@email.com', password:'504090se', location: 'Akron, OH')
+u1 = User.create!(username: 'ElChapo', email: 'thebeardedone@email.com', password:'stepback', location: 'Los Angeles, CA')
+u1 = User.create!(username: 'KingJames', email: 'akidfromakron@email.com', password:'kdisasnake', location: 'Cleveland, OH')
+u1 = User.create!(username: 'easymoneysnip', email: 'burner@email.com', password:'imachampion2x', location: 'Washington, DC')
+u1 = User.create!(username: 'DameTime', email: 'betterthancurry@email.com', password:'best3shooter', location: 'Oakland, CA')
+u1 = User.create!(username: '23donkey', email: 'draygreen@email.com', password:'naturalmotion', location: 'Saginaw, MI')
