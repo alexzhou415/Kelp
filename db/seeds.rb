2f4f3c859bfb569190ff569316ea824ceb425c6a
alexzhou415 <zhoualex10@yahoo.com>
2021-04-12 14:06:56 -0700

    Change to aws seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 
 User.destroy_all
 Business.destroy_all
+
 
 u1 = User.create!(username: 'DemoUser', email: 'demo@email.com', password:'12345678', location: 'Richmond, CA')
 u1 = User.create!(username: 'JRich', email: 'jjsmith@email.com', password:'hotdog64', location: 'San Diego, CA')
@@ -24,5 +26,11 @@ squid = User.create!(username: 'Squidward Tentacles', email: 'clarinetlover@seam
 star = User.create!(username: 'Patrick Star', email: 'pinhead@seamail.com', password:'ismayoaninstrument', location: 'Bikini Bottom, PO')
 puff = User.create!(username: 'Poppy Puff', email: 'mrspuff@seamail.com', password:'tetrodotoxin', location: 'Bikini Bottom, PO')
 
-Business.create!(name: 'Krusty Krab', creator_id: krabs.id, address: '831 Bottom Feeder Lane, Bikini Bottom, Pacific Ocean', rating: 5, category: "Restaurants")
-Business.create!(name: "Mrs. Puff's Boating School", creator_id: puff.id, address: '233 Barnacle Ave, Bikini Bottom, Pacific Ocean', rating: 5, category: "Education")
+b1 = Business.create!(name: 'Krusty Krab', creator_id: krabs.id, address: '831 Bottom Feeder Lane, Bikini Bottom, Pacific Ocean', rating: 5, category: "Restaurants")
+b2 = Business.create!(name: "Mrs. Puff's Boating School", creator_id: puff.id, address: '233 Barnacle Ave, Bikini Bottom, Pacific Ocean', rating: 5, category: "Education")
+
+file1 = open('https://kelp-seeds.s3-us-west-1.amazonaws.com/krusty_krab.png')
+b1.photo.attach(io: file1, filename: 'krusty_krab.png')
+
+file2 = open('https://kelp-seeds.s3-us-west-1.amazonaws.com/boating_school.png')
+b2.photo.attach(io: file2, filename: 'boating_school.png')
