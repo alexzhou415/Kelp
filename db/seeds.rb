b7072d3caae079576b11908e936f19dc662d0b3f
alexzhou415 <zhoualex10@yahoo.com>
2021-04-12 17:44:06 -0700

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,28 +11,27 @@ User.destroy_all
 Business.destroy_all
 
 
-u1 = User.create!(username: 'DemoUser', email: 'demo@email.com', password:'12345678', location: 'Richmond, CA')
-u1 = User.create!(username: 'JRich', email: 'jjsmith@email.com', password:'hotdog64', location: 'San Diego, CA')
-u1 = User.create!(username: 'StevenAdams', email: 'newzealander@email.com', password:'matemate', location: 'New York, NY')
-u1 = User.create!(username: 'ChefCurry', email: '40percent@email.com', password:'504090se', location: 'Akron, OH')
-u1 = User.create!(username: 'ElChapo', email: 'thebeardedone@email.com', password:'stepback', location: 'Los Angeles, CA')
-u1 = User.create!(username: 'KingJames', email: 'akidfromakron@email.com', password:'kdisasnake', location: 'Cleveland, OH')
-u1 = User.create!(username: 'easymoneysnip', email: 'burner@email.com', password:'imachampion2x', location: 'Washington, DC')
-u1 = User.create!(username: 'DameTime', email: 'betterthancurry@email.com', password:'best3shooter', location: 'Oakland, CA')
-u1 = User.create!(username: '23donkey', email: 'draygreen@email.com', password:'naturalmotion', location: 'Saginaw, MI')
 krabs = User.create!(username: 'Eugene Krabs', email: 'mrkrabs@seamail.com', password:'moneybags', location: 'Bikini Bottom, PO')
 sponge = User.create!(username: 'Spongebob Squarepants', email: 'frycook@seamail.com', password:'icanboat', location: 'Bikini Bottom, PO')
 squid = User.create!(username: 'Squidward Tentacles', email: 'clarinetlover@seamail.com', password:'hatemyjob', location: 'Bikini Bottom, PO')
 star = User.create!(username: 'Patrick Star', email: 'pinhead@seamail.com', password:'ismayoaninstrument', location: 'Bikini Bottom, PO')
 puff = User.create!(username: 'Poppy Puff', email: 'mrspuff@seamail.com', password:'tetrodotoxin', location: 'Bikini Bottom, PO')
 
+
 b1 = Business.create!(name: 'Krusty Krab', creator_id: krabs.id, address: '831 Bottom Feeder Lane, Bikini Bottom, Pacific Ocean', rating: 5, category: "Restaurants")
 b2 = Business.create!(name: "Mrs. Puff's Boating School", creator_id: puff.id, address: '233 Barnacle Ave, Bikini Bottom, Pacific Ocean', rating: 5, category: "Education")
-
+b3 = Business.create!(name: "Larry's Gym", creator_id: star.id, address: '9387 Krill Blvd, Bikini Bottom, Pacific Ocean', rating: 4, category: "Active Life")
+b4 = Business.create!(name: "Weenie Hut Jr's", creator_id: star.id, address: '256 Hotdog Road, Bikini Bottom, Pacific Ocean', rating: 4.5, category: "Restaurants")
 
 file1 = open('https://kelp-seeds.s3-us-west-1.amazonaws.com/krusty_krab.png')
 b1.photo.attach(io: file1, filename: 'krusty_krab.png')
 
 file2 = open('https://kelp-seeds.s3-us-west-1.amazonaws.com/boating_school.png')
 b2.photo.attach(io: file2, filename: 'boating_school.png')
+
+file3 = open("https://kelp-seeds.s3-us-west-1.amazonaws.com/Larry's_Gym.png")
+b3.photo.attach(io: file3, filename: "Larry's_Gym.png")
+
+file4 = open('https://kelp-seeds.s3-us-west-1.amazonaws.com/weenie_hut_jr.png')
+b4.photo.attach(io: file4, filename: 'weenie_hut_jr.png')
 
